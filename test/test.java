d0efb3a4d1396357091df792d0c954071a27d6c2
spencer596 <spencer5980@gmail.com>
2024-11-20 15:23:16 -0700

    file 0 works but others dont

--- test/test.java
+++ test/test.java
@@ -1,12 +1,14 @@
 package test;
 
 import tm.TMSimulator;
+import tm.TMState;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedHashSet;
 import java.util.List;
 
 public class test{
@@ -17,7 +19,7 @@ public class test{
 
         TMSimulator tm = new TMSimulator();
 
-        String filePath = "test/file0.txt";
+        String filePath = "test/file5.txt";
 
         List<String> partsList = new ArrayList<>();
         try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
@@ -46,17 +48,54 @@ public class test{
         for (int i = 0; i < states; i++){
             tm.addState(String.valueOf(i));
         }
+        LinkedHashSet<TMState> idk = tm.getAllStates();
+        System.out.println("sttates" + idk);
 
         int alphabet = Integer.parseInt(partsList.get(1));
 
         for (int i = 0; i < alphabet+1; i++){
             tm.addSigma(i);
         }
+        System.out.println("sigma is " + tm.getSigma());
+        // things to check what state we are in
+        int index = 0;
+        int sigmaIndex = 0;
+        // if state has all sigmas then do stuff idk
         tm.setStart("0");
-        tm.setFinal(String.valueOf(alphabet));
+        tm.setFinal(String.valueOf(states - 1));
+        for (int i = 2; i < partsList.size()-1; i++){
+            if (sigmaIndex == tm.getMaxSigma()+1){
+                sigmaIndex = 0;
+                index++;
+            }
+            if (tm.isFinal(String.valueOf(index))){
+                System.out.println("the index is " + index);
+                break;
+            }
+            String[] parts = partsList.get(i).split(",");
+//            System.out.println("parts " + Arrays.toString(parts));
+//            System.out.println("parts 1 " + parts[0]);
+
+            tm.addTransition(String.valueOf(index),parts[0],parts[2].charAt(0), (char) sigmaIndex,parts[1].charAt(0));
+            sigmaIndex++;
+            System.out.println("sigma " + sigmaIndex);
+
+
+
+
+        }
+
+        tm.printTransitionTable();
+
 
         System.out.println(tm.getAllStates());
         System.out.println(tm.getSigma());
+        System.out.println(partsList.get(2));
+        System.out.println("last part " + partsList.get(partsList.size()-1));
+
+//        ArrayList<Character> tape = tm.step(partsList.get(partsList.size()-1));
+        ArrayList<Character> tape = tm.step("");
+        System.out.println(tape);
 
 
     }
--- tm/TMSimulator.java
+++ tm/TMSimulator.java
@@ -1,18 +1,26 @@
 package tm;
 
-import java.util.HashMap;
-import java.util.LinkedHashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class TMSimulator {
 
+    public class Transition{
+        TMState nextState;
+        char writeSymbol;
+        char direction; // 'L' for left, 'R' for right, 'S' for stay
 
+        Transition(TMState nextState, char writeSymbol, char direction) {
+            this.nextState = nextState;
+            this.writeSymbol = writeSymbol;
+            this.direction = direction;
+        }
+    }
     LinkedHashSet<Integer> sigma;
     LinkedHashSet<TMState> states;
     String startState;
     LinkedHashSet<TMState> finalStates;
-    Map<TMState, Map<Character, Set<TMState>>> transitionTable;
+    Map<TMState, Map<Character, Transition>> transitionTable;
 
     public TMSimulator() {
 
@@ -23,6 +31,28 @@ public class TMSimulator {
         transitionTable = new HashMap<>();
         sigma.add(0);
 
+    }
+
+    public void printTransitionTable() {
+        System.out.println("Transition Table:");
+        TMState test = getState(startState);
+        System.out.println(test);
+        for (Map.Entry<TMState, Map<Character, Transition>> stateEntry : transitionTable.entrySet()) {
+            TMState currentState = stateEntry.getKey();
+            System.out.println(stateEntry);
+            System.out.println("\nFrom State: " + currentState.getName());
+
+            Map<Character, Transition> transitions = stateEntry.getValue();
+            for (Map.Entry<Character, Transition> transitionEntry : transitions.entrySet()) {
+                char readSymbol = transitionEntry.getKey();
+                Transition transition = transitionEntry.getValue();
+                System.out.printf("  Read: (%d) → State: %s, Write: %c, Move: %c%n",
+                        (int)readSymbol,
+                        transition.nextState.getName(),
+                        transition.writeSymbol,
+                        transition.direction);
+            }
+        }
     }
 
 
@@ -58,6 +88,96 @@ public class TMSimulator {
         return false;
     }
 
+    public void addTransition (String fromState, String toState, Character direction,
+                               Character read, Character write){
+
+
+        TMState fS = getState(fromState);
+        System.out.println("from state " + fromState);
+        TMState tS = getState(toState);
+
+        if (fS == null || tS == null){
+            System.out.println("null while adding trans");
+            return;
+        }
+
+        transitionTable.putIfAbsent(fS, new HashMap<>());
+
+        Transition transition = new Transition(tS, write, direction);
+        transitionTable.get(fS).put(read, transition);
+        System.out.println("Keys in transition table: " +
+                transitionTable.get(fS).keySet().stream()
+                        .map(c -> String.valueOf((int)c))
+                        .collect(Collectors.joining(", ")));
+
+
+    }
+
+
+    // needs a thing where if the input empty then still do it i guess
+    public ArrayList<Character> step(String input) {
+
+        int headPosition = 1;
+        ArrayList<Character> tape = new ArrayList<>();
+        tape.add('0');
+
+        for (char c : input.toCharArray()) {
+            tape.add(c);
+        }
+
+        tape.add('0');
+
+        TMState currentState = getState(startState);
+
+        while (!finalStates.contains(currentState)) {
+            char currentSymbol = tape.get(headPosition);
+            char sym = (char)(currentSymbol - '0' ); // covert to numeric
+            System.out.println("Current State: " + currentState.getName());
+            System.out.println("Current Symbol: " + currentSymbol);
+            System.out.println("sym: " + sym);
+
+            Map<Character, Transition> stateTransitions = transitionTable.get(currentState);
+
+            if (stateTransitions == null) {
+                System.out.println("No valid transition found");
+                break;
+            }
+//            for (Map.Entry<Character, Transition> transitionEntry : stateTransitions.entrySet()) {
+//                char readSymbol = transitionEntry.getKey();
+//                Transition transition = transitionEntry.getValue();
+//                System.out.printf("  Read: (%d) → State: %s, Write: %c, Move: %c%n",
+//                        (int)readSymbol,
+//                        transition.nextState.getName(),
+//                        transition.writeSymbol,
+//                        transition.direction);
+//            }
+
+            Transition transition = stateTransitions.get(currentSymbol);
+            System.out.println(transition);
+            tape.set(headPosition, transition.writeSymbol);
+
+            switch (transition.direction) {
+                case 'L' -> {
+                    headPosition--;
+                    if (headPosition < 0) {
+                        tape.add(0, '0');
+                        headPosition = 0;
+                    }
+                }
+                case 'R' -> {
+                    headPosition++;
+                    if (headPosition >= tape.size()) {
+                        tape.add('0');
+                    }
+                }
+            }
+
+            currentState = transition.nextState;
+            System.out.println(" tape now " + tape);
+        }
+        return tape;
+    }
+
 
     public void addSigma(int sym) {
         for (int i = 0; i < sym; i++) {
@@ -68,6 +188,10 @@ public class TMSimulator {
     }
     public Set<Integer> getSigma() {
         return sigma;
+    }
+
+    public int getMaxSigma(){
+        return Collections.max(sigma);
     }
 
     public boolean isFinal(String name) {
